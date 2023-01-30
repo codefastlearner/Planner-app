f8737e055045e2db0e405e147b3c7db9dd611a82
Ciprian <ciprian.oltean23@gmail.com>
2023-01-30 12:24:02 +0000

    creating html elements

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
 let currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(currentDate);
+
+$(".saveBtn").on("click", saveEvent);
+
+function saveEvent() {
+    let event = $(this).siblings(".description").val();
+    console.log("event= ", event);
+}
